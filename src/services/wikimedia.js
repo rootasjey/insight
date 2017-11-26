c4d7b5d3b7611fc9a88c5e35b736012a8c0744bd
emmanuelJd <elisa.emmanuel.jd@gmail.com>
2017-11-26 17:51:08 +0100

    try to be cool and change xmlHttpRequest to fetch method

--- src/services/wikimedia.js
+++ src/services/wikimedia.js
@@ -1,31 +1,15 @@
 const Wikimedia = {
   search: function (keywords) {
-    fetch({
-      url: `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${keywords}&origin=*`
-    })
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${keywords}&origin=*`)
     .then(function (result) {
-      console.log(result)
-      return result
-    }, function (reason) {
+      result.json().then(function (data) {
+        console.log(data)
+      })
+    })
+    .catch(function (reason) {
       console.log(reason)
-      return reason
     })
   }
-}
-
-function fetch (arg) {
-  return new Promise((resolve, reject) => {
-    const method = arg.method || 'GET'
-    const xhr = new XMLHttpRequest()
-
-    xhr.open(method, arg.url, true)
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-
-    xhr.onload = () => resolve(xhr.responseText)
-    xhr.onerror = () => reject(xhr.statusText)
-
-    xhr.send(JSON.stringify(arg.data))
-  })
 }
 
 export default Wikimedia
